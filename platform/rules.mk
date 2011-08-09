4d4aaec1a7dc42788d6cb4825214332f7ddb3d27
yliao <yiran.liao@iessys.com>
2011-08-09 13:51:57 +0800

    Minor changes. 	modified:   platform/rules.mk 	new file:   support/template/rules.mk

--- platform/rules.mk
+++ platform/rules.mk
@@ -1,6 +1,8 @@
 PLATFORM_INCLUDES := -I$(PLATFORM_PATH)
 
-# TODO Add specific platform related module
+# TODO Add specific platform related module.
+#      Should only include one platform module each time.
+
 ifeq ($(MCU), STM32F103RB)
 include $(PLATFORM_PATH)/stm32/rules.mk
 endif
