74263a1d74243f68c4c8f87cc6727ac657575555
librae <librae8226@gmail.com>
2011-08-10 08:51:39 +0800

    Minor change. 	modified:   sample/main.c

--- sample/main.c
+++ sample/main.c
@@ -1,3 +1,21 @@
+/* *****************************************************************************
+ * @file    sample/main.c
+ *
+ * @brief   sample code
+ * @date    2011-08-10
+ *
+ * @history 2011-08-10 Initial revision
+ *
+ * *****************************************************************************
+ * COPYRIGHT (C) LEAFGRASS - Librae (librae8226@gmail.com)
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ * ****************************************************************************/
+
 #include <stdio.h>
 
 #include <ousia/scheduler.h>
