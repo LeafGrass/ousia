a088cf2a5af4ee34101b4b4fa3d73ed77453e64c
librae <librae8226@gmail.com>
2011-08-14 02:31:37 +0800

    Add some comment.

---
+++ core/port/ousia_port.h
@@ -0,0 +1,21 @@
+/* *****************************************************************************
+ * @file    core/port/ousia_port.c
+ *
+ * @brief   porting code
+ *
+ * @log     2011-08-14 Initial revision
+ *
+ * *****************************************************************************
+ * COPYRIGHT (C) LEAFGRASS - Librae (librae8226@gmail.com)
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ * ****************************************************************************/
+
+#ifndef __OUSIA_PORT_H__
+#define __OUSIA_PORT_H__
+
+#endif /* OUSIA_PORT_H */
