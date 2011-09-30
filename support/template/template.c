0724df8c46aa1d8b55a9f9c46d58d1ead6d87158
librae <librae8226@gmail.com>
2011-09-30 10:09:50 +0800

    Add codestyle document & update license information.

--- support/template/template.c
+++ support/template/template.c
@@ -1,20 +1,29 @@
-/* *****************************************************************************
- * @file    support/template/template.c
+/*
+ * Copyright (c) 2011-2012 LeafGrass <leafgrass.g@gmail.com>
+ * All rights reserved.
  *
- * @brief   File template.
- *
- * @log     2011-07-28 Initial revision
- *          2011-07-29 Second revision
- *
- * *****************************************************************************
- * COPYRIGHT (C) LEAFGRASS - LeafGrass (leafgrass.g@gmail.com)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
  * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
  * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
  * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- * ****************************************************************************/
+ */
+
+/*
+ * @file    support/template/template.c
+ * @brief   File template.
+ * @log     2011.7 Initial revision
+ *          2011.7 Second revision
+ */
 
 #include <template.h>
 
--- support/template/template.h
+++ support/template/template.h
@@ -1,20 +1,29 @@
-/* *****************************************************************************
- * @file    support/template/template.h
+/*
+ * Copyright (c) 2011-2012 LeafGrass <leafgrass.g@gmail.com>
+ * All rights reserved.
  *
- * @brief   File template.
- *
- * @log     2011-07-28 Initial revision
- *          2011-07-29 Second revision
- *
- * *****************************************************************************
- * COPYRIGHT (C) LEAFGRASS - LeafGrass (leafgrass.g@gmail.com)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
  * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
  * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
  * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- * ****************************************************************************/
+ */
+
+/*
+ * @file    support/template/template.h
+ * @brief   File template.
+ * @log     2011.7 Initial revision
+ *          2011.7 Second revision
+ */
 
 #ifndef __TEMPLATE_H__
 #define __TEMPLATE_H__
@@ -27,7 +36,7 @@
  * @return  an integer
  * @note    just use it and take it easy
  */
-int sample_function(int a, int b, int *pResult);
+int sample_function(int a, int b, int *result);
 
 #endif /* __TEMPLATE_H__ */
 
