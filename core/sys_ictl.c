5f1abf1c8f030de037f92ddd22d6d8fa311cfdbc
librae <librae8226@gmail.com>
2011-10-04 17:46:58 +0800

    Standardize codestyle.

--- core/sys_ictl.c
+++ core/sys_ictl.c
@@ -1,19 +1,28 @@
-/* *****************************************************************************
- * @file    core/sys_ictl.c
+/*
+ * Copyright (c) 2011-2012 LeafGrass <leafgrass.g@gmail.com>
+ * All rights reserved.
  *
- * @brief   core IRQ controller
- *
- * @log     2011.8 initial revision
- *
- * *****************************************************************************
- * COPYRIGHT (C) LEAFGRASS - LeafGrass (leafgrass.g@gmail.com)
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
  * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
  * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
  * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- * ****************************************************************************/
+ */
+
+/*
+ * @file    core/sys_ictl.c
+ * @brief   core IRQ controller
+ * @log     2011.8 initial revision
+ */
 
 #include <ousia/ousia.h>
 #include <ousia/ousia_type.h>
