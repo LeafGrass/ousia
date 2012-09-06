1bd6eef033ff9e15dda1ff3361ba18199035c5aa
Librae <librae8226@gmail.com>
2012-09-06 13:18:10 +0800

    [core/mm] prepare memory management part

---
+++ core/sys_mm.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2011-2012 LeafGrass <leafgrass.g@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ * OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ * ARM SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ */
+
+/*
+ * @file    core/sys_mm.c
+ * @brief   core memory management
+ * @log     2012.9 initial revision
+ */
+
+#include <ousia/ousia.h>
+#include <ousia/ousia_type.h>
+#include <port/ousia_port.h>
+#include <sys/mm.h>
+
+/*
+ * @brief   sample function
+ * @param   a -i- an integer
+ *          b -i- another integer
+ *          pResult -o- result
+ * @return  an integer
+ * @note    just use it and take it easy
+ */
