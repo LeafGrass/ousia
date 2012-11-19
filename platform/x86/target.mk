e10c7bf772f8344fc374c50490aabbe5f9c79dfa
Librae <librae8226@gmail.com>
2012-11-19 17:39:29 +0800

    [plat/x86] discard obj files' size output

--- platform/x86/target.mk
+++ platform/x86/target.mk
@@ -1,10 +1,10 @@
 $(BUILD_PATH)/$(OUSIA_TARGET): $(BUILDDIRS) $(TGT_BIN) $(USR_BUILDDIRS) $(USR_TGT_BIN)
 	$(SILENT_LD) $(CC) $(LDFLAGS) -o $@ $(TGT_BIN) $(USR_TGT_BIN) -Wl,-Map,$(BUILD_PATH)/$(OUSIA_TARGET).map
 	$(SILENT_DISAS) $(DISAS) -d $(BUILD_PATH)/$(OUSIA_TARGET) > $(BUILD_PATH)/$(OUSIA_TARGET).disas
-	@echo " "
-	@echo "Object file sizes:"
+#	@echo " "
+#	@echo "Object file sizes:"
 	@find $(BUILD_PATH) -iname *.o | xargs $(SIZE) -t > $(BUILD_PATH)/$(OUSIA_TARGET).sizes
-	@cat $(BUILD_PATH)/$(OUSIA_TARGET).sizes
+#	@cat $(BUILD_PATH)/$(OUSIA_TARGET).sizes
 	@echo " "
 	@echo "Final Size:"
 	@$(SIZE) $(BUILD_PATH)/$(OUSIA_TARGET)
