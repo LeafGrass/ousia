76fd4049dfab70828a0abb059a2fd3efe27ff732
Librae <librae8226@gmail.com>
2013-03-21 16:43:00 +0800

    plat: stm32: remove trivial script code

--- platform/stm32/target.mk
+++ platform/stm32/target.mk
@@ -3,10 +3,7 @@ $(BUILD_PATH)/$(OUSIA_TARGET): $(BUILD_PATH)/$(OUSIA_TARGET).bin
 $(BUILD_PATH)/$(OUSIA_TARGET).bin: $(BUILD_PATH)/$(OUSIA_TARGET).elf
 	$(SILENT_OBJCOPY) $(OBJCOPY) -v -Obinary $(BUILD_PATH)/$(OUSIA_TARGET).elf $@ 1>/dev/null
 	$(SILENT_DISAS) $(DISAS) -d -x $(BUILD_PATH)/$(OUSIA_TARGET).elf > $(BUILD_PATH)/$(OUSIA_TARGET).lss
-#	@echo " "
-#	@echo "Object file sizes:"
 	@find $(BUILD_PATH) -iname *.o | xargs $(SIZE) -t > $(BUILD_PATH)/$(OUSIA_TARGET).sizes
-#	@cat $(BUILD_PATH)/$(OUSIA_TARGET).sizes
 	@echo " "
 	@echo "Final Size:"
 	@$(SIZE) $<
